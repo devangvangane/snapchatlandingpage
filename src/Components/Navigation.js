e9cf3d2bbd2cc90cf7152a5933c47c570b9a475b
Devang Vangane <131960193+devangvangane@users.noreply.github.com>
2024-01-31 13:45:17 +0530

    Update Navigation.js

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -10,29 +10,29 @@ function Navigation() {
     <div className='nav'>
         <div className='logosearch'>
             <button className='snap_b' type='button' onClick={reloadPage}> 
-                <img className='logo' src='images/snapchat.png' alt='logo'></img>
+                <img className='logo' src='./images/snapchat.png' alt='logo'></img>
             </button>
             <div className='searchinput'>
-                <img className='searchimg' src='/images/search.png' alt='search'></img>
+                <img className='searchimg' src='./images/search.png' alt='search'></img>
                 <input type='search' placeholder='Search'></input>
             </div>
         </div>
         <div className='slsc'>
             
             <button className='but'>
-                <img className='stories' src='/images/stories.png' alt='stories'></img>
+                <img className='stories' src='./images/stories.png' alt='stories'></img>
                 <p> Stories</p>
             </button>
             <button className='but'>
-                <img className='spotlight' src='/images/spotlight.png' alt='spotlight'></img>
+                <img className='spotlight' src='./images/spotlight.png' alt='spotlight'></img>
                 <p>Spotlight</p>
             </button>
             <button className='but'>
-                <img className='chat' src='/images/chat.JPG' alt='chat'></img>
+                <img className='chat' src='./images/chat.JPG' alt='chat'></img>
                 <p> Chat</p>
             </button>
             <button className='but'>
-                <img  className='lenses' src='/images/lenses.png' alt='lenses'></img>
+                <img  className='lenses' src='./images/lenses.png' alt='lenses'></img>
                 <p> Lenses</p>
             </button>
         </div>
